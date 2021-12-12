809c04bf793054dd5e03dd99455d4e9b23cf92d0
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2021-12-12 23:50:33 +0800

    kernel-based mrf

--- src/hmrf.jl
+++ src/hmrf.jl
@@ -396,8 +396,8 @@ function logPrior!(model::Union{MRFBatchSeg{T}, PairedMRFBatchSeg{T}}, k::Int) w
     Ek = view(model.E2, :, k)
     @inbounds for v ∈ 1:model.n
         #Rk[v] -= sum([(model.seg[idx] != k) * model.f[v][kkk] for (kkk, idx) ∈ enumerate(model.adj[v])])
-        #Ek[v] = -model.ω * sum( (model.seg[collect(model.adj[v])] .!= k) .* model.f[v] )
-        Ek[v] = -model.ω * sum((model.seg[collect(model.adj[v])] .!= k))
+        Ek[v] = -model.ω * sum( (model.seg[collect(model.adj[v])] .!= k) .* model.f[v] )
+        #Ek[v] = -model.ω * sum((model.seg[collect(model.adj[v])] .!= k))
     end
     #Rk .+= log(model.nk[k]/model.n)
 end
