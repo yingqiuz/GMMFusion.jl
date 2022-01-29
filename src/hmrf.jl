ff41b504f5c24c678c7d94d60c8ea7243a1d5f56
Ying-Qiu Zheng <yingqiuz12fdu@gmail.com>
2022-01-29 19:13:45 +0800

    add new mrf model

--- src/hmrf.jl
+++ src/hmrf.jl
@@ -442,7 +442,7 @@ function logPrior!(model::Union{MRFBatch{T}, PairedMRFBatch{T}}) where T <: Real
         @inbounds for v ∈ 1:model.n
             #Rk[v] += model.ω * sum([model.R[idx, k] for idx ∈ model.adj[v]])
             #Ek[v] = model.ω * sum(model.R[collect(model.adj[v]), k] .* model.f[v])
-            Ek[v] = -model.ω * sum((model.E1[collect(model.adj[v]), k] .- model.E1[v, k]) .^ 2)
+            Ek[v] = -model.ω * sum(    model.E1[collect(model.adj[v]), [kk for kk in 1:model.K if kk != k]]    )
         end
     end
     #model.E2 ./= sum(model.E2, dims=2)
